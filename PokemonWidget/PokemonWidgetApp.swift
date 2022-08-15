9165884a08ed2626871e27965eae9a1b302d4abf
Oncu <oohancan@gmail.com>
2022-08-15 19:03:46 +0300

    Initial Commit

---
+++ PokemonWidget/PokemonWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidgetApp.swift
+//  PokemonWidget
+//
+//  Created by Oncu Can on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
