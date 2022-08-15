90ee1d8d236dfc973fe9d2e3657cf2b7c590b92b
Oncu <oohancan@gmail.com>
2022-08-15 20:18:52 +0300

    add AppStorage and func userDefaultsSave

--- PokemonWidget/View/ContentView.swift
+++ PokemonWidget/View/ContentView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("pokemon", store: UserDefaults(suiteName: "group.com.mrxx7.PokemonWidget"))
+    
+    var pokemonData: Data = Data()
+    
     var body: some View {
         VStack {
             ForEach(allPokemon) { pokemon in
                 PokemonView(pokemon: pokemon)
+                    .onTapGesture(perform: {
+                    userDefaultsSave(pokemon: pokemon)
+                })
             }
+        }
+    }
+    
+    func userDefaultsSave(pokemon: Pokemon) {
+        if let pokemonData = try?
+            JSONEncoder().encode(pokemon) {
+            self.pokemonData = pokemonData
+            print(pokemon.name)
         }
     }
 }
